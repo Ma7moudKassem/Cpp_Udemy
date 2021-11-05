3b6f512e4a26bf85a8b853162290a3357c24a2b9
modyk <modykassem123@gmail.com>
2021-11-05 04:57:04 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/kassem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kassem.dir/main.cpp.obj"
+  "kassem.exe"
+  "kassem.exe.manifest"
+  "kassem.pdb"
+  "libkassem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kassem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+
+using namespace std;
+int main ()
+
+{
+//    int x , y ;
+//    cout<<"enter first number:";
+//    cin>> x;
+//    cout<<"enter second number:";
+//    cin>> y;
+//    cout<<"x="<<x<<"\t";
+//    cout<<"y="<<y<<endl;
+//    cout<<"sum="<<x/y;
+
+//----------------program to show even and oop numbers ----------------//
+
+/*
+int x ;
+cout<<"Enter Your Number:";
+cin>>x;
+
+if(x%2 == 0)
+{
+    cout<<"This Number is Even";
+} else{
+    cout<<"This Number is Opp";
+}
+ */
+
+//----------------------------------------------------//
+
+/*
+int x,y;
+cout<<"Enter height:";
+cin>>x;
+cout<<"Enter width:";
+cin>>y;
+cout<<"Space="<<x*y<<"CM^2";
+*/
+
+//----------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cout<<"mahmoud kassem";
+
+
+
+    return 0 ;
+}
